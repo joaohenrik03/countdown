6ce848187931bb54e78aa63099bfc7ccd2ab4805
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-04 17:25:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const currentYear = new Date().getFullYear();
 const currentDay = new Date().getDay();
 
-const toLaunch = `${currentDay + 1} Jun ${currentYear}`;
+const releaseDateOf = `${currentDay + 1} Jun ${currentYear}`;
 
 function countdown() {
-    const toLaunchIn = new Date(toLaunch);
+    const forLaunch = new Date(releaseDateOf);
     const currentDate = new Date();
 
-    const allSeconds = (toLaunchIn - currentDate) / 1000;
+    const allSeconds = (forLaunch - currentDate) / 1000;
     const days = Math.floor(allSeconds / 3600 / 24);
     const hours = Math.floor(allSeconds / 3600) % 24;
     const minutes = Math.floor(allSeconds / 60) %60; 
@@ -21,4 +21,4 @@ function countdown() {
     document.querySelector('#seconds').innerHTML = seconds < 10 ? '0'+seconds : seconds;
 }
 
-setInterval(countdown, 100)
+setInterval(countdown, 100);
