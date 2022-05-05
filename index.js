bb4ecae62c1ecf81ae17349366bc7bf93170514c
João Henrik Signori Zilch <84729916+joaohenrik03@users.noreply.github.com>
2022-05-04 22:48:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Countdown
 
-const currentYear = new Date().getFullYear();
-const currentDay = new Date().getDay();
-
-const releaseDateOf = `${currentDay + 1} Jun ${currentYear}`;
+const releaseDateOf = `11 jun 2022`; // Sua data de lançamento aqui // Your release date here
 
 function countdown() {
     const forLaunch = new Date(releaseDateOf);
@@ -14,11 +11,18 @@ function countdown() {
     const hours = Math.floor(allSeconds / 3600) % 24;
     const minutes = Math.floor(allSeconds / 60) %60; 
     const seconds = Math.floor(allSeconds) % 60;
-
-    document.querySelector('#days').innerHTML = days < 10 ? '0'+days : days;
-    document.querySelector('#hours').innerHTML = hours < 10 ? '0'+hours : hours;
-    document.querySelector('#minutes').innerHTML = minutes < 10 ? '0'+minutes : minutes;
-    document.querySelector('#seconds').innerHTML = seconds < 10 ? '0'+seconds : seconds;
+    
+    if (days < 0 || hours < 0 || minutes < 0 || seconds < 0) {
+        document.querySelector('#days').innerHTML = '00';
+        document.querySelector('#hours').innerHTML = '00';
+        document.querySelector('#minutes').innerHTML = '00';
+        document.querySelector('#seconds').innerHTML = '00';
+    } else {
+        document.querySelector('#days').innerHTML = days < 10 ? '0'+days : days;
+        document.querySelector('#hours').innerHTML = hours < 10 ? '0'+hours : hours;
+        document.querySelector('#minutes').innerHTML = minutes < 10 ? '0'+minutes : minutes;
+        document.querySelector('#seconds').innerHTML = seconds < 10 ? '0'+seconds : seconds;
+    }  
 }
 
 setInterval(countdown, 100);
